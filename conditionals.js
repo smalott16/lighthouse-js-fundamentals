3c66a8b6ff0865b8ca77df5f14b5b7388ab016ef
Spencer Malott <spencermalott16@gmail.comgit config --global user.email spencermalott16@gmail.com>
2021-08-04 03:17:16 +0000

    testing if statements on their own

---
+++ conditionals.js
@@ -0,0 +1,12 @@
+const raining = true;
+const cold = false;
+
+if (raining){
+  console.log("Don't forget your ubmrella!");
+}
+
+if (cold){
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
