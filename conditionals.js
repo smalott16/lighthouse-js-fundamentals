2f4935a66af8dc7101e3dd4005d6bfbaa2fd57f5
Spencer Malott <spencermalott16@gmail.comgit config --global user.email spencermalott16@gmail.com>
2021-08-04 03:40:30 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,8 +1,11 @@
 const raining = true;
 const cold = false;
 const temperature = 12;
+const isCitizen = true;
+const age = 26;
 
-/*if (raining) {
+/* IF STATEMENT
+if (raining) {
   console.log("Don't forget your umbrella!");
 }
 
@@ -10,14 +13,17 @@ if (cold) {
   console.log("Make sure you pick out a scarf!");
 }
 
-console.log("Now you're ready to go outside!"); */
+console.log("Now you're ready to go outside!"); 
+*/
 
-/*if (cold) {
+/* IF ELSE STATEMENT
+if (cold) {
   console.log("Make sure you pick out a scarf");
 } else {
   console.log("Short sleeves are fine");
 }*/
 
+/* IF ELSE IF ELSE STATEMENT
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
 } else if (temperature < 15) {
@@ -27,3 +33,17 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+*/
+
+
+if (isCitizen && age > 18){
+  console.log("You are eligible to vote");
+}
+
+if (temperature <-40 || temperature >40){
+  console.log("Maybe going outside isn't such a good idea");
+}
+
+if (!raining){
+  console.log("Leave your umbrella at home!")
+}
