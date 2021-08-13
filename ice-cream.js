867c36b52e6cd50fc24b2e5c33fcae06f7336b46
Spencer Malott <spencermalott16@gmail.comgit config --global user.email spencermalott16@gmail.com>
2021-08-13 03:17:34 +0000

    first few files working with arrays

---
+++ ice-cream.js
@@ -0,0 +1,17 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+//Add a flavour
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+//Get the first flavour
+console.log(iceCreamFlavours[0]);
+
+//Get the last flavour
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+
+//Get the number of flavours
+console.log(iceCreamFlavours.length);
